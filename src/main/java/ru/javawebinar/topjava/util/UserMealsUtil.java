91d5e66759063bf8eab3bc6c1ab2e72710511ddf
zixitix <zixitix@gmail.com>
2019-07-11 21:59:32 +0600

    Last commit

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -27,6 +27,7 @@ public class UserMealsUtil {
     public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
         // TODO return filtered list with correctly exceeded field
 
+
         return null;
     }
 }
