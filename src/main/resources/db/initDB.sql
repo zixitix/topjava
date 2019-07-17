50d2ec30fda5c7a3a5740a0a9eb642d80aa3e960
zixitix <zixitix@gmail.com>
2019-07-18 01:08:04 +0600

    solution

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
 
@@ -22,4 +23,14 @@ CREATE TABLE user_roles
   role    VARCHAR,
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE meals
+(
+    id               SERIAL    PRIMARY KEY,
+    description      VARCHAR                 NOT NULL,
+    date_time        TIMESTAMP               NOT NULL,
+    calories         INTEGER                 NOT NULL,
+    user_id          INTEGER                 NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,4 +1,5 @@
 DELETE FROM user_roles;
+DELETE FROM meals;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
@@ -9,3 +10,10 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+INSERT INTO meals (description,date_time,calories, user_id) VALUES
+('User meal first', '2016-12-22 20:10:25-07',2000, 100000),
+('User meal second','2016-06-12 17:10:25-07',2000, 100000),
+('User meal third','2016-06-27 15:10:25-07',1000, 100000),
+('Admin meal first','2016-05-30 11:10:25-07',1500, 100001),
+('Admin meal second','2016-04-02 12:10:25-07',800, 100001),
+('Admin meal third','2016-01-22 22:10:25-07',1400, 100001);
